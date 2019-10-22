9c6597c862af8557c0c3a5e937c6e1314e7c55f0
Hamel Husain <hamel.husain@gmail.com>
2019-10-21 20:20:58 -0700

    fix

--- action_files/entrypoint.sh
+++ action_files/entrypoint.sh
@@ -2,3 +2,4 @@
 # see https://repo2docker.readthedocs.io/en/latest/usage.html
 
 echo "jupyter-repo2docker --no-build --ref $GITHIB_SHA $GITHUB_WORKSPACE"
+jupyter-repo2docker --no-build --ref $GITHIB_SHA $GITHUB_WORKSPACE
