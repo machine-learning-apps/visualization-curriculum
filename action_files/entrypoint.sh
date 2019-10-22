444b48605f6725a25d75de95869b87dc0dda648d
Hamel Husain <hamel.husain@gmail.com>
2019-10-21 20:33:20 -0700

    fix

--- action_files/entrypoint.sh
+++ action_files/entrypoint.sh
@@ -2,9 +2,9 @@
 # see https://repo2docker.readthedocs.io/en/latest/usage.html
 
 
-echo ${DOCKER_PASSWORD} | docker login -u $DOCKER_USERNAME --password-stdin
+echo ${INPUT_DOCKER_PASSWORD} | docker login -u $INPUT_DOCKER_USERNAME --password-stdin
 
 ls $GITHUB_WORKSPACE
 
-cmd="jupyter-repo2docker --no-build --push hamelsmu/viz-notebook:$GITHUB_SHA $GITHUB_WORKSPACE"
+cmd="jupyter-repo2docker --no-build --push hamelsmu/viz-notebook:${GITHUB_SHA} $GITHUB_WORKSPACE"
 echo "repo2docker command: $cmd"
